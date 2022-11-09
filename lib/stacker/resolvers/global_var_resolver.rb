dc77c6d68276ae2657899c7bfbef467299b25e8a
Emma OBrien <emmao@goforward.com>
2022-11-09 11:26:23 -0800

    use ref

--- lib/stacker/resolvers/global_var_resolver.rb
+++ lib/stacker/resolvers/global_var_resolver.rb
@@ -10,8 +10,7 @@ module Stacker
         global_vars = YAML.load_file(global_defaults)
         puts(global_vars)
         puts(ref)
-        puts(ref.fetch('GlobalVar'))
-        global_vars[ref.fetch('GlobalVar')]
+        global_vars[ref]
       end
 
     end
