4d7b08b0be0e116403d9f3f4d30f3460461807c3
Emma OBrien <emmao@goforward.com>
2022-11-09 11:24:03 -0800

    debug

--- lib/stacker/resolvers/global_var_resolver.rb
+++ lib/stacker/resolvers/global_var_resolver.rb
@@ -8,6 +8,9 @@ module Stacker
       def resolve
         global_defaults = region.defaults.fetch 'globalParameterFile', {}
         global_vars = YAML.load_file(global_defaults)
+        puts(global_vars)
+        puts(ref)
+        puts(ref.fetch('GlobalVar'))
         global_vars[ref.fetch('GlobalVar')]
       end
 
